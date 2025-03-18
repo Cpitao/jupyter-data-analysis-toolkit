e142390da40688451080bb61812f566270397ff5
dmoskalewicz <dmoskalewicz@tutanota.com>
2025-03-18 10:03:59 +0100

    removed tensorflow from default dependencies

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 # Install dependencies
 uv venv --python 3.11
 source .venv/bin/activate
-uv pip install pandas polars numpy tensorflow
+uv pip install pandas polars numpy
 
 # Start Jupyter
 uv run --with jupyter jupyter lab
