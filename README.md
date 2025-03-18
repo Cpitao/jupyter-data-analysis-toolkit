ebaf692455d9b6529756beb0c180d176cc278e29
Cpitao <pio.ignacy@gmail.com>
2025-03-18 11:29:32 +0100

    add checks, update readme

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Setup script that creates a Jupyter data analysis environment
 ## Setup
 
 ### Linux
+Create the environment in a default directory:
 ```sh
 curl -LsSf https://raw.githubusercontent.com/dmoskalewicz/jupyter-data-analysis-toolkit/refs/heads/main/setup.sh | bash
+```
+
+Create the environment in `directory-name` directory:
+```sh
+curl -sSL https://raw.githubusercontent.com/dmoskalewicz/jupyter-data-analysis-toolkit/refs/heads/main/setup.sh | bash -s -- directory-name
 ```
--- setup.sh
+++ setup.sh
@@ -1,13 +1,27 @@
+#!/bin/bash
+
+# Set default directory name
+default_dir="jupyter-data-analysis-toolkit"
+
+# Check if directory name is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "No directory name provided. Using default: $default_dir"
+    project_dir="$default_dir"
+else
+    project_dir="$1"
+fi
+
 # Create project directory
-mkdir jupyter-data-analysis-toolkit
-cd jupyter-data-analysis-toolkit
+mkdir "$project_dir"
+cd "$project_dir"
 
-# Install uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
-
-# Conditionally add ~/.local/bin to PATH
-if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
-  export PATH="$PATH:$HOME/.local/bin"
+# Check if uv is already installed
+if ! command -v uv &> /dev/null; then
+    echo "uv is not installed. Installing now..."
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+    export PATH=$PATH:~/.local/bin
+else
+    echo "uv is already installed."
 fi
 
 # Install dependencies
